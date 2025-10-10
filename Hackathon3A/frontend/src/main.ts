d58348b3852729ee455674b9cda3418c3c0b2cd3
Alex <alex.letondor@outlook.fr>
2025-10-10 22:04:44 +0200

    babylon init

--- Hackathon3A/frontend/src/main.ts
+++ Hackathon3A/frontend/src/main.ts
@@ -1,24 +1,37 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import {
+  Engine,
+  Scene,
+  ArcRotateCamera,
+  Vector3,
+  HemisphericLight,
+  MeshBuilder,
+} from "@babylonjs/core";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const engine = new Engine(canvas, true);
+
+const scene = new Scene(engine);
+
+const camera = new ArcRotateCamera(
+  "Camera",
+  Math.PI / 2,
+  Math.PI / 2,
+  4,
+  Vector3.Zero(),
+  scene
+);
+camera.attachControl(canvas, true);
+
+const light = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+
+const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
+
+engine.runRenderLoop(() => {
+  scene.render();
+});
+
+window.addEventListener("resize", () => {
+  engine.resize();
+});
+
