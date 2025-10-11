f65a0151dd701edfbc3a801b7de3ef8e616c160e
cyraullie <cyril.goldenschue@gmail.com>
2025-10-11 10:57:21 +0200

    add chat part

--- Hackathon3A/frontend/src/main_page.ts
+++ Hackathon3A/frontend/src/main_page.ts
@@ -1,6 +1,7 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 import { getMapFileData } from './get_map.ts'
+import chat_bot from './chat-bot.svg'
+import close_icon from './close.svg'
 
 export async function run() {
 	const { methods, map2D } = await getMapFileData(1);
@@ -12,6 +13,9 @@ export async function run() {
 			<a id="methods_list">list of methods enable</a>
 		</div>
 		<div id="center_part">
+			<button class="chatbot" type="button">
+				<img id="chat_icon" src="${chat_bot}" alt="chat bot logo" />
+			</button>
 			<div id="game">
 				game
 			</div>
@@ -23,11 +27,37 @@ export async function run() {
 					prompt
 				</div>
 			</div>
+			<div id="chat">
+				chat
+			</div>
 		</div>
 	</div>
 	`
 	const methodsList = document.querySelector<HTMLAnchorElement>('#methods_list')!
 	methodsList.textContent = methods.join(', ')
-	setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+	
+	// Récupère les éléments
+	const chatButton = document.querySelector<HTMLButtonElement>('.chatbot')!
+	const chatIcon = document.querySelector<HTMLImageElement>('#chat_icon')!
+	const leftPart = document.querySelector<HTMLDivElement>('#left_part')!
+	const chatDiv = document.querySelector<HTMLDivElement>('#chat')!
 
+	let chatVisible = false
+
+	// Toggle au clic
+	chatButton.addEventListener('click', () => {
+		chatVisible = !chatVisible
+
+		if (chatVisible) {
+			leftPart.style.display = 'none'
+			chatDiv.style.display = 'block'
+			chatIcon.src = close_icon // → change l'image
+			chatIcon.alt = "close chat"
+		} else {
+			leftPart.style.display = 'flex'
+			chatDiv.style.display = 'none'
+			chatIcon.src = chat_bot // → remet l'image du chatbot
+			chatIcon.alt = "open chat"
+		}
+	})
 }
